7a171a7cc4f1cdc0f53b956d10364e53ba7dcc6d
Furkan Emre <furkan54emre@gmail.com>
2019-02-28 23:21:36 +0300

    Snap Defenders to Grid

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -23,6 +23,16 @@ public class DefenderSpawner : MonoBehaviour
         Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
 
-        return worldPos;
+        var gridPos = SnapToGrid(worldPos);
+
+        return gridPos;
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+
+        return new Vector2(newX, newY);
     }
 }
